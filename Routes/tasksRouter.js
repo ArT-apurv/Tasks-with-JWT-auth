00efcecd6bde65e524aa61f83824b2bf1498d83e
ArT-apurv <89764232+ArT-apurv@users.noreply.github.com>
2022-05-24 16:04:53 +0530

    added login routes

--- Routes/tasksRouter.js
+++ Routes/tasksRouter.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  login,
   getTask,
   getAllTasks,
   updateTask,
@@ -10,7 +9,6 @@ const {
   createTask,
 } = require("../controllers/tasksController");
 
-router.route("/login").post(login);
 router.route("/tasks").get(getAllTasks).post(createTask);
 router.route("/tasks/:id").get(getTask).patch(updateTask).delete(deleteTask);
 module.exports = router;
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const tasks = require("./Routes/tasksRouter");
+const authRouter = require("./Routes/authRoutes");
+const tasksRouter = require("./Routes/jobsRoutes");
 const connectDB = require("./Database/Connect");
 require("dotenv").config();
 const notFound = require("./middleware/notFound");
@@ -9,7 +10,8 @@ const Ports = 3001;
 app.use(express.json());
 app.use(express.static("./public"));
 
-app.use("/api/v1", tasks);
+app.use("/api/v1/tasks", tasksRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFound);
 
